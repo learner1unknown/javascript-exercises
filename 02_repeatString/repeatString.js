1fe6eca2b7b1d6d7ff63b9e0e790643a21f7ad7f
Reuben Frith <reubenfrith007@gmail.com>
2022-04-05 22:02:29 +1000

    ex02 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    let word = []
+
+    if (num < 0) {
+        return 'ERROR'
+    }
+    else {
+
+    for (let i = 0; i < num; i++) {
+        word.push(string);
+    }
+
+    return word.join('')
+    }
+    
 };
 
 // Do not edit below this line
