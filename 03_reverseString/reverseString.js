299fa25a0be73b48c274bd8c9175c5c739db00e2
Reuben Frith <reubenfrith007@gmail.com>
2022-04-05 22:13:06 +1000

    ex03 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,18 @@
-const reverseString = function() {
+const reverseString = function(word) {
+
+    // split string up
+    // reverse string
+    // join reversed string
+
+    word = word.split('');
+    let reversed_word = [];
+
+    for (let i = 0; i < word.length + 1; i++) {
+        reversed_word[i] = word[word.length - i];
+    }
+
+    return reversed_word.join('')
+
 
 };
 
