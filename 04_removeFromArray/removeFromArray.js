9c8b1eb52b7e1791defffc6f0160ac524bad244f
Reuben Frith <reubenfrith007@gmail.com>
2022-04-06 21:34:39 +1000

    ex04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
 
+    let args = Array.from(arguments);
+    array = args[0];
+    args.shift();
+
+    let results = array.filter((array) => !args.includes(array));
+    
+    return results
+
 };
 
 // Do not edit below this line
