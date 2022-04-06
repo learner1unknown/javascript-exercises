66460d6c52d63e8a2f43af06c3fa006cf69629a7
Reuben Frith <reubenfrith007@gmail.com>
2022-04-06 22:07:55 +1000

    ex05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,32 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+
+    let counter = 0;
+    let begin;
+    let finish;
+
+    // ensure only numbers are entered
+    if (typeof(start) == 'number' && typeof(end) == 'number') {
+
+        if (start < 0 || end < 0) {
+            return 'ERROR'
+        }
+        else if (end > start) {
+            begin = start;
+            finish = end;
+        }
+        else if (end <= start){
+            begin = end;
+            finish = start;
+        }
+       
+
+    } else { return 'ERROR'}
+
+    for ( let i = begin ; i <= finish; i++) {
+        counter = counter + i;
+    }
+
+    return (counter);
 
 };
 
