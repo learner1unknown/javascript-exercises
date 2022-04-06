2ba3bf8b5e0e765cb50fcbdf702ae63f1fb6e254
Reuben Frith <reubenfrith007@gmail.com>
2022-04-06 22:30:44 +1000

    ex07 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const ftoc = function() {
+const ftoc = function(farhenheit) {
+
+  let celsius = (farhenheit - 32) * 5/9;
+ 
+  return Number(celsius.toFixed(1))
+
 
 };
 
-const ctof = function() {
+const ctof = function(celsius) {
+
+  let farhenheit = celsius * 9/5 + 32;
+
+  return Number(farhenheit.toFixed(1))
 
 };
 
