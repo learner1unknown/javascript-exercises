fa08d0005c538e9aeef17ce6172e57c8f1402503
Reuben Frith <reubenfrith007@gmail.com>
2022-04-07 20:14:03 +1000

    ex09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+// remove all spaces and nonalphanumeric characters 
+// make string lowercase and break up into characters -> this is the original string
+// reverse the original string and store in separate variable
+// compare original string and reversed string, if equal = true i.e. is a palindrome
+
+string = string.replace(/\W|_/g, '');
+string = string.toLowerCase();
+string = string.split('');
+
+let reversed = [];
+
+for (let i = 0; i <= string.length; i++) {
+    reversed[i] = string[string.length - i];
+}
+
+return reversed.join('') == string.join('')
 
 };
 
