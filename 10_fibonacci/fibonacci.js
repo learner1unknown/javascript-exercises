6034836f40077b6fcef9d6c6f9a8bfe8cf8f5500
Reuben Frith <reubenfrith007@gmail.com>
2022-04-07 20:26:24 +1000

    ex10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if (typeof(n) == 'number') {
+        if (n > 0) {
+    let Phi = (1+5**(1/2))/2;
+    let phi = (1-5**(1/2))/2;
+
+    return Math.round((Phi**n - phi**n) / 5**(1/2))
+        }
+        else {return 'OOPS'}
+    }
+    else {return 1}
 };
 
 // Do not edit below this line
