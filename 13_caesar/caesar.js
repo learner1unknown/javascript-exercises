990512c92b074be23ecaeabf8d031d740ded7671
Reuben Frith <reubenfrith007@gmail.com>
2022-05-17 22:37:36 +1000

    completed ex 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,53 @@
 const caesar = function() {
 
+    const string = arguments[0];
+    let code = arguments[1];
+    if (code > 26 || code < -26) {
+        code = code - Math.round(code/26)*26;
+    }
+  
+    const letters = string.split('');
+    //const ascii = letters.map(letter => letter.charCodeAt())
+    //console.log(ascii)
+
+    const ascii = letters.map(letter => {
+
+        let number = letter.charCodeAt();
+        
+        if( number >= 65 && number <= 90) {
+            // change UPPER case letter 
+            number = number + code;
+            // check if adding number will exceed 90 = 'Z' if it does remove 26
+            if(number > 90) {
+                number = number - 26;
+            }
+            // check if adding number will be less than 65 = 'A' if it does add 26
+            else if(number < 65) {
+                number = number + 26;
+            }
+        }
+        else if ( number >= 97 && number <= 122) {
+            // change lower case letter
+            number = number + code;
+            // check if adding number will exceed 122 = 'z' if it does remove 26
+            if(number > 122) {
+                number = number - 26;
+            }
+            // check if adding number will be less than 97 = 'a' if it does add 26
+            else if(number < 97) {
+                number = number + 26;
+            }
+        }
+
+        return number
+        // keep number the same if it is not a letter as it is a non alphabetic character
+
+
+    })
+
+    return String.fromCharCode.apply(String, ascii)
+    
+
 };
 
 // Do not edit below this line
